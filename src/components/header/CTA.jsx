708b44e09a8de3cbd4fce713e0ee3bce8e356aaa
utkars3 <97681722+utkars3@users.noreply.github.com>
2023-09-29 22:09:00 +0530

    Updated resume link

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,7 +4,8 @@ import CV from '../../assets/Resume.pdf'
 const CTA = () => {
   return (
     <div className='cta'>
-      <a href={CV} download className='btn'>Download CV</a>
+{/*       <a href={CV} download className='btn'>Download CV</a> */}
+      <a href="https://drive.google.com/file/d/1nc26CLVLBzybXuE_kqNtfCuOrffKytth/view?usp=drive_link" download className='btn'>Download CV</a>
       <a href="#contact" className='btn btn-primary'>Let's Talk </a>
     </div>
   )
