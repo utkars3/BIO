e3590aed9c40e03255e958426e4479d871a88638
utkars3 <utkarshkesharwani3@gmail.com>
2024-12-05 03:22:34 +0530

    changes to resume link and page title

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,8 +4,7 @@ import CV from '../../assets/Resume.pdf'
 const CTA = () => {
   return (
     <div className='cta'>
-{/*       <a href={CV} download className='btn'>Download CV</a> */}
-      <a href="https://drive.google.com/file/d/1nc26CLVLBzybXuE_kqNtfCuOrffKytth/view?usp=drive_link" download className='btn'>Download CV</a>
+      <a href="https://drive.google.com/file/d/1to6h0QhsnaWTiMEsQgt8V5CAkYyolf5G/view?usp=drive_link" download className='btn'>Download CV</a>
       <a href="#contact" className='btn btn-primary'>Let's Talk </a>
     </div>
   )
